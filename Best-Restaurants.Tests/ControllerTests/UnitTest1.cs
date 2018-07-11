96d6ccc208af8d2dd637c707234f7b538f8883a1
Kevin Ahn and Matt Smith <matt@>
2018-07-11 08:36:31 -0700

    Add initial commit

---
+++ Best-Restaurants.Tests/ControllerTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Best_Restaurants.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
